a2a33272980e7198e15d88b30f860bf1c0791634
AnotherDroog <36770425+AnotherDroog@users.noreply.github.com>
2019-03-22 13:58:04 +0700

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name="noma",
-    version="0.1",
+    version="0.1.1",
     packages=["noma"],
     install_requires=['psutil', 'docopt', 'requests', 'docker', 'docker-compose'],
     entry_points={
