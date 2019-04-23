883fcb84e6740093188bb6a66f2cbf2c9b33ab4e
AnotherDroog <36770425+AnotherDroog@users.noreply.github.com>
2019-04-23 14:29:57 +0700

    .travis.yml: Fix PyPI URL

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name="noma",
-    version="0.2.7",
+    version="0.2.8",
     packages=["noma"],
     install_requires=['psutil', 'docopt', 'requests'],
     entry_points={
