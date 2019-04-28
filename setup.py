6cd41897f4599d16d4d69c84c071f00e53154f7d
nolim1t <hello@nolim1t.co>
2019-04-28 14:40:48 +0700

    Increment version

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name="noma",
-    version="0.3.9",
+    version="0.3.10",
     packages=["noma"],
     install_requires=["psutil", "docopt", "requests"],
     entry_points={"console_scripts": ["noma = noma.noma:main"]},
     # metadata to display on PyPI
     zip_safe=True,
-    author="AnotherDroog",
-    author_email="another_droog@protonmail.com",
+    author="LNCM Contributors",
+    author_email="lncm@protonmail.com",
     description="bitcoin lightning node management tool",
     license="Apache 2.0",
     keywords="bitcoin lightning node",
