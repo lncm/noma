1317d3660f0a41cb9b2c3fe0fa21d93193f3abf6
AnotherDroog <36770425+AnotherDroog@users.noreply.github.com>
2019-06-07 12:30:23 +0700

    Move vagrant installation to install.sh

--- Vagrantfile
+++ Vagrantfile
@@ -3,12 +3,8 @@
 Vagrant.configure("2") do |config|
 
   config.vm.box = "generic/alpine39"
+  config.vm.hostname = "alpine-vagrant"
   config.vm.synced_folder ".", "/vagrant", type: "rsync", rsync__exclude: ".git/"
 
-  config.vm.provision "shell", inline: <<-SHELL
-    apk add python3 py3-psutil
-    cd /vagrant
-    python3 setup.py develop
-    python3 /vagrant/tests/test_usb.py
-  SHELL
+  config.vm.provision "shell", path: "install.sh"
 end
---
+++ install.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+# install noma and dependencies within alpine linux 
+# or run with "vagrant up" to create alpine VM
+
+alpine_install() 
+{
+    # noma dependencies
+    apk add python3 py3-psutil
+    # docker
+    apk add docker
+    service docker start
+    # docker-compose
+    apk add libffi py3-cffi python3-dev build-base python3 openssl-dev
+    pip3 install docker-compose
+    # install noma
+    python3 setup.py develop
+    # test noma
+    python3 tests/test_usb.py
+    python3 tests/test_install.py
+    # run noma
+    noma --version
+    noma --help
+}
+
+install_git() 
+{
+    apk add git
+    git clone https://github.com/lncm/noma.git
+    cd noma || exit
+    alpine_install
+}
+
+vagrant_install()
+{
+    if [ -f "/vagrant" ]; then
+        cd /vagrant || exit
+        alpine_install
+    else
+        install_git
+    fi
+}
+
+# chroot_install() 
+# {
+#     # TODO: alpine-chroot and then install()
+# }
+
+check_vm() 
+{
+    if [ -f "/vagrant" ]; then
+        echo "detected vagrant VM"
+        vagrant_install
+    else
+        echo "fetching noma sources"
+        install_git
+    fi
+}
+
+main()
+{
+    if [ -f "/etc/alpine-release" ]; then
+        check_vm
+    else
+        # TODO: Create chroot here on Ubuntu
+        echo "Error: not an alpine linux system!"
+        exit 1
+    fi
+}
+main
