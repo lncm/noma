cc81d9bff0294a08d1f7aec895c4ba909e23f772
Another Droog <36770425+AnotherDroog@users.noreply.github.com>
2019-08-22 14:00:46 +0700

    Reduce exposed cli functionality

--- noma/noma.py
+++ noma/noma.py
@@ -6,7 +6,7 @@ Usage:
   noma stop
   noma check
   noma logs
-  noma lnd info
+  noma info
   noma lnd create
   noma lnd backup
   noma lnd restore
@@ -94,25 +94,25 @@ def lnd(args):
         else:
             print("lnd is already stopped")
 
-    elif args["connect"]:
-        print(
-            call(
-                [
-                    "docker",
-                    "exec",
-                    "compose_lnd_1",
-                    "lncli",
-                    "connect",
-                    args["<address>"],
-                ]
-            )
-        )
+    # elif args["connect"]:
+    #     print(
+    #         call(
+    #             [
+    #                 "docker",
+    #                 "exec",
+    #                 "compose_lnd_1",
+    #                 "lncli",
+    #                 "connect",
+    #                 args["<address>"],
+    #             ]
+    #         )
+    #     )
 
     elif args["autoconnect"]:
         lnd.autoconnect(args["<path>"])
-
-    elif args["lncli"]:
-        call(["docker", "exec", "compose_lnd_1", "lncli", args["<command>"]])
+    #
+    # elif args["lncli"]:
+    #     call(["docker", "exec", "compose_lnd_1", "lncli", args["<command>"]])
 
     elif args["logs"]:
         if args["--tail"]:
@@ -125,16 +125,16 @@ def lnd(args):
             )
         else:
             node.logs("lnd")
-
-    elif args["info"]:
-        call(["docker", "exec", "compose_lnd_1", "lncli", "getinfo"])
+    #
+    # elif args["info"]:
+    #     call(["docker", "exec", cfg.LND_MODE + "_lnd_1", "lncli", "getinfo"])
 
     elif args["create"]:
         lnd.check_wallet()
 
-    elif args["unlock"]:
-        # manually unlock lnd wallet
-        call(["docker", "exec", "-it", "compose_lnd_1", "lncli", "unlock"])
+    # elif args["unlock"]:
+    #     # manually unlock lnd wallet
+    #     call(["docker", "exec", "-it", "compose_lnd_1", "lncli", "unlock"])
 
     elif args["autounlock"]:
         lnd.autounlock()
@@ -162,10 +162,7 @@ def node(args):
     from subprocess import call
 
     if args["info"]:
-        call(
-            ["docker", "exec", "compose_bitcoind_1", "bitcoin-cli", "-getinfo"]
-        )
-        call(["docker", "exec", "compose_lnd_1", "lncli", "getinfo"])
+        node.info()
 
     elif args["start"]:
         if node.is_running("bitcoind"):
@@ -177,7 +174,7 @@ def node(args):
     elif args["stop"]:
         node.stop_daemons()
         # now we can safely stop
-        call(["service", "docker-compose", "stop"])
+        # call(["service", "docker-compose", "stop"])
 
     # elif args["restart"]:
     #     node.stop_daemons()
@@ -186,87 +183,87 @@ def node(args):
 
     elif args["logs"]:
         node.logs()
-
-    elif args["temp"]:
-        print(node.temp())
-
-    elif args["voltage"]:
-        node.voltage(args["<device>"])
-
-    elif args["freq"]:
-        node.freq(args["<device>"])
-
-    elif args['turbo']:
-        print("Set CPU scaling")
-        from subprocess import DEVNULL
-        for cpu_num in range(0, 4):
-            device_path = "/sys/devices/system/cpu/cpu{n}/cpufreq/scaling_governor".format(n=cpu_num)
-            call(["echo", "ondemand > {p} ".format(p=device_path)], shell=True, stdout=DEVNULL, stderr=DEVNULL)
-
-    elif args['memory']:
-        print(node.memory(args['<device>']))
-
-    elif args["backup"]:
-        node.backup()
-
-    elif args["restore"]:
-        print("restore unimplemented")
-
-    elif args["source"]:
-        node.install_git()
-        node.get_source()
-
-    elif args["diff"]:
-        node.do_diff()
-
-    elif args["reinstall"]:
-        print("Warning: this will wipe data on your SD card!")
-        if args["--confirm"]:
-            if args["--full"]:
-                node.full_reinstall()
-            else:
-                node.reinstall()
-        else:
-            print("You must pass --confirm to continue")
-
-    elif args["tunnel"]:
-        node.tunnel(args["<port>"], args["<host>"])
-
-    elif args["usb-setup"]:
-        import noma.install
-
-        noma.install.usb_setup()
-
-    elif args["install-box"]:
-        import noma.install
-
-        noma.install.install_box()
-
-    elif args["create-swap"]:
-        from noma import install
-        print(install.create_swap())
-
+    #
+    # # elif args["temp"]:
+    # #     print(node.temp())
+    #
+    # elif args["voltage"]:
+    #     node.voltage(args["<device>"])
+    #
+    # elif args["freq"]:
+    #     node.freq(args["<device>"])
+    #
+    # elif args['turbo']:
+    #     print("Set CPU scaling")
+    #     from subprocess import DEVNULL
+    #     for cpu_num in range(0, 4):
+    #         device_path = "/sys/devices/system/cpu/cpu{n}/cpufreq/scaling_governor".format(n=cpu_num)
+    #         call(["echo", "ondemand > {p} ".format(p=device_path)], shell=True, stdout=DEVNULL, stderr=DEVNULL)
+    #
+    # elif args['memory']:
+    #     print(node.memory(args['<device>']))
+    #
+    # elif args["backup"]:
+    #     node.backup()
+    #
+    # elif args["restore"]:
+    #     print("restore unimplemented")
+    #
+    # elif args["source"]:
+    #     node.install_git()
+    #     node.get_source()
+    #
+    # elif args["diff"]:
+    #     node.do_diff()
+    #
+    # elif args["reinstall"]:
+    #     print("Warning: this will wipe data on your SD card!")
+    #     if args["--confirm"]:
+    #         if args["--full"]:
+    #             node.full_reinstall()
+    #         else:
+    #             node.reinstall()
+    #     else:
+    #         print("You must pass --confirm to continue")
+    #
+    # elif args["tunnel"]:
+    #     node.tunnel(args["<port>"], args["<host>"])
+    #
+    # elif args["usb-setup"]:
+    #     import noma.install
+    #
+    #     noma.install.usb_setup()
+    #
+    # elif args["install-box"]:
+    #     import noma.install
+    #
+    #     noma.install.install_box()
+    #
+    # elif args["create-swap"]:
+    #     from noma import install
+    #     print(install.create_swap())
+    #
     elif args["check"]:
         node.check()
-
-    elif args["devtools"]:
-        node.devtools()
-
-    elif args["status"]:
-        if node.is_running("bitcoind"):
-            print("bitcoind is running")
-        else:
-            print("bitcoind is not running")
-        if node.is_running("lnd"):
-            print("lnd is running")
-        else:
-            print("lnd is not running")
-
-    elif args["swap"]:
-        node.get_swap()
-
-    elif args["ram"]:
-        node.get_ram()
+    #
+    # elif args["devtools"]:
+    #     node.devtools()
+    #
+    # elif args["status"]:
+    #     if node.is_running("bitcoind"):
+    #         print("bitcoind is running")
+    #     else:
+    #         print("bitcoind is not running")
+    #     if node.is_running("lnd"):
+    #         print("lnd is running")
+    #     else:
+    #         print("lnd is not running")
+    #
+    # elif args["swap"]:
+    #     node.get_swap()
+    #
+    # elif args["ram"]:
+    #     node.get_ram()
 
 
 def main():
