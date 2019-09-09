fb4a73b8b4be000348671dc88458782ce0842e93
Another Droog <36770425+AnotherDroog@users.noreply.github.com>
2019-09-10 00:20:38 +0700

    Add debian installation routine

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-# install noma and dependencies within alpine linux
-# or run with "vagrant up" to create alpine VM
+# install noma and dependencies on Alpine and
+# Debian-based Linux systems or run with
+# "vagrant up" to create VM
 
 alpine_install() {
     # noma dependencies
@@ -14,11 +15,36 @@ alpine_install() {
     pip3 install docker-compose
     # install noma
     python3 setup.py develop
+}
+
+run_noma() {
     # run noma
     noma --version || exit 1
     noma --help || exit 1
     run_tests || exit 1
     start_noma
+}
+
+debian_install() {
+    if [ -x "$(command -v apt-get)" ]; then
+        # docker
+        curl -fsSL https://get.docker.com -o get-docker.sh
+        sh get-docker.sh
+        # dependencies
+        apt-get install python3-pip python3-cffi libffi-dev libssl-dev git -y
+        # reload profile to update PATH
+        source ~/.profile
+        # python virtual environment
+        pip3 install virtualenv
+        virtualenv /media/noma/venv
+        source /media/noma/venv/bin/activate
+        # docker-compose
+        pip3 install docker-compose
+        # noma
+        python3 setup.py develop
+    else
+        echo "Error: apt-get not available"
+    fi
 }
 
 start_noma() {
@@ -45,9 +71,9 @@ install_git() {
     apk add git
     git clone https://github.com/lncm/noma.git
     if [ ! -f setup.py ]; then
-        cd noma || exit
+        cd noma || exit 1
     fi
-    alpine_install
+    check_os
 }
 
 chroot_install() {
@@ -57,33 +83,42 @@ chroot_install() {
         || exit 1
     chmod +x ./alpine-chroot-install || exit 1
     ./alpine-chroot-install -d /alpine-v310 -b v3.10
-    /alpine-v310/enter-chroot install.sh
+    /alpine-v310/enter-chroot ./install.sh
 }
 
-check_vm() {
+check_os() {
+    if [ -f "/etc/alpine-release" ]; then
+        alpine_install || exit 1
+    elif [ -x "$(command -v apt-get)" ]; then
+        debian_install || exit 1
+    else
+        echo "Not an Alpine or Debian-based Linux system"
+        echo
+        echo "Attempting to create Alpine chroot"
+        chroot_install || exit 1
+    fi
+}
+
+check_root() {
+if ! [ $(id -u) = 0 ]; then
+   echo "Error: You must be root to install"
+   exit 1
+fi
+}
+
+main() {
+    check_root
     if [ -d "/media/noma" ]; then
-        # Vagrant mode
         echo "Detected source directory"
-        alpine_install
+        # Vagrant mode
+        check_os
     else
         echo "Sources not found, fetching from github..."
         mkdir /media
         mkdir /media/noma
-        cd /media/noma || exit
+        cd /media/noma || exit 1
         install_git
     fi
-}
-
-main() {
-    if [ -f "/etc/alpine-release" ]; then
-        check_vm
-    else
-        echo "Not an alpine linux system!"
-        echo
-        echo "Attempting to create alpine chroot"
-        chroot_install || exit 1
-        check_vm
-
-    fi
+    run_noma
 }
 main
