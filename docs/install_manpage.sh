df966f478ab5e1127291f5fb606e574ec37edd1b
Another Droog <36770425+AnotherDroog@users.noreply.github.com>
2019-10-20 16:09:50 +0700

    install_manpage.sh: Compatibility changes for Alpine

--- docs/install_manpage.sh
+++ docs/install_manpage.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 # build and install manpage for noma
 set -eu
 
 MANPAGE="noma.1"
 MAN_DIR="/usr/local/man/man1"
+MAN_DIR_ALPINE="/usr/share/man/man1"
 
 
 check_root() {
@@ -21,11 +22,23 @@ check_directory() {
     fi
 }
 
+alpine_os() {
+    if [ -f "/etc/alpine-release" ]; then
+        return 0
+    fi
+}
+
 check_root
 check_directory
+
+if [ alpine_os ]; then
+    MAN_DIR=$MAN_DIR_ALPINE
+fi
 
 cd docs
 sphinx-build . _build -b man
 install -g 0 -o 0 -m 0644 _build/$MANPAGE $MAN_DIR
 gzip ${MAN_DIR}/${MANPAGE}
-mandb
+if [ ! alpine_os ]; then
+    mandb
+fi
